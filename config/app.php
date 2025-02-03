a9830f6ace404084487483b185ef4ece8153e7e0
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-03 20:40:25 +0700

    12. Tighencoziggy

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tightenco\Ziggy\ZiggyServiceProvider::class,
 
     ],
 
@@ -210,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Route' => Tightenco\Ziggy\Ziggy::class,
     ])->toArray(),
 
 ];
