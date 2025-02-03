60da02826333e03615560abfe39987b1946f38b2
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-03 14:31:17 +0700

    6. Definisikan table isi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // mendefinisikan kolom table yang bisa diisi
+    protected $fillable = [
+        'title', 'body'
+    ];
 }
