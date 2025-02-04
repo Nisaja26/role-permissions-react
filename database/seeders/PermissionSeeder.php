43af10d2e7a7ccd432be557b64a3647ac842b1b7
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-04 13:42:39 +0700

    7. import model permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// impor model role dari paket permission
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    // mendefinisikan metode run ketika seeder di jalankan
+    public function run()
     {
-        //
+        // membuat izin baru
+        $createPost = Permission::create(['name' => 'create_post']);
+        $editPost = Permission::create(['name' => 'edit_post']);
+        $deletePost = Permission::create(['name' => 'delete_post']);
+
+        // membuat peran baru
+        $role = Role::create(['name' => 'editor']);
+        // memberikan izin sebelumnya
+        $role->givePermissionTo($createPost, $editPost, $deletePost);
     }
 }
