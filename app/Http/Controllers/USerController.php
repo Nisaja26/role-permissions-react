593c666ecc302e61f70b5190dbff948de14bfcf3
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-04 13:47:08 +0700

    8. Make usercontroller

---
+++ app/Http/Controllers/USerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // mengambil semua data dari model user
+        $users = User::with('roles')->get();
+        return Inertia::render('Users/Index', ['users' => $users]);
+    }
+    public function editUser($id)
+    {
+        $user = User::find($id);
+
+        // memeriksa apakah user ditemukan
+        if (!$user) {
+            return Inertia::render('404')->with('message', 'User not found')->status(404);
+        }
+
+        $editorRole = 'editor';
+
+        if ($user->hasRole($editorRole)) {
+            $user->removeRole($editorRole);
+        } else {
+            $user->assignRole($editorRole);
+        }
+        return redirect(route('user.index'))->with('success', 'Role Updated.');
+    }
+}
