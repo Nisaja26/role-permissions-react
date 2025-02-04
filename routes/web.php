10c799285d1878774a9cd74cfb166376abb0cdda
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-04 13:50:15 +0700

    9. Make rute usercontroller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+// masukkan usercontroller
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -30,6 +32,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);
+// masukkan rute usercontroller
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
+Route::put('/users/{id}/edit', [UserController::class, 'editUser'])->name('user.edit');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
